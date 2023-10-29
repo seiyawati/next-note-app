2854079acecac56642dfb65639824280bd4bc59b
Seiya Kawamoto <seiya.kawamoto@enigmo.co.jp>
2023-10-29 16:48:14 +0900

    ノートの作成ページの実装

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from "@/globals/db";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { zUpsertNote } from "@/app/notes/type";
 
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
 // 'force-dynamic': Force dynamic rendering and uncached data fetching of a layout or page by disabling all caching of fetch requests and always revalidating. This option is equivalent to:
@@ -7,5 +8,16 @@ export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const notes = await prisma.note.findMany();
+
   return NextResponse.json(notes);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const parcedData = zUpsertNote.parse(data);
+  const note = await prisma.note.create({
+    data: { title: parcedData.title, body: parcedData.body },
+  });
+
+  return new NextResponse(`${note.id}`, { status: 201 })
 }
